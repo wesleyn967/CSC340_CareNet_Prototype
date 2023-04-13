0f82a773f869cbb3067d1fd3df6192774697988f
Wesley Nguyen <w_nguyen@uncg.edu>
2023-04-13 15:42:19 -0400

    CareNet v4.0

--- CareNet/CareNet/src/main/java/com/csc340sp23/CareNet/provider/ProviderController.java
+++ CareNet/CareNet/src/main/java/com/csc340sp23/CareNet/provider/ProviderController.java
@@ -1,84 +1,97 @@
-    package com.csc340sp23.CareNet.provider;
+package com.csc340sp23.CareNet.provider;
 
-    import com.csc340sp23.CareNet.provider.ProviderInfo;
-    import com.csc340sp23.CareNet.provider.ProviderService;
-    import org.springframework.beans.factory.annotation.Autowired;
-    import org.springframework.stereotype.Controller;
-    import org.springframework.ui.Model;
-    import org.springframework.web.bind.annotation.GetMapping;
-    import org.springframework.web.bind.annotation.ModelAttribute;
-    import org.springframework.web.bind.annotation.PostMapping;
+import com.csc340sp23.CareNet.provider.ProviderInfo;
+import com.csc340sp23.CareNet.provider.ProviderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
-    @Controller
-    public class ProviderController {
+@Controller
+public class ProviderController {
 
-        @Autowired
-        private ProviderService providerService;
+    @Autowired
+    private ProviderService providerService;
 
-        @GetMapping("/providerLoginPage")
-        public String providerLogin(Model model) {
-            model.addAttribute("providerInfo", new ProviderInfo());
-            return "providerLoginPage";
-        }
-
-       @GetMapping("/providerLoginPageRetry")
-        public String providerLoginRetry (Model model) {
-            return "providerLoginPageRetry";
-        } 
-
-        @PostMapping("/providerLoginPage")
-        public String processLoginForm(@ModelAttribute("providerInfo") ProviderInfo providerInfo) {
-            boolean authenticated = providerService.authenticate(providerInfo.getUsername(), providerInfo.getPassword());
-            if (authenticated && providerInfo.getUserType().equals("provider")) {
-                return "redirect:/providerDashboard";
-            } else {
-                return "redirect:/providerLoginPageRetry";
-            }
-        }
-
-        @GetMapping("/providerDashboard")
-        public String showDashboardPage() {
-
-            return "providerDashboard";
-        }
-
-        @GetMapping("/providerAppointment")
-        public String showAppointmentPage(Model model) {
+    @GetMapping("/providerLoginPage")
+    public String providerLogin(Model model) {
         model.addAttribute("providerInfo", new ProviderInfo());
-            return "providerAppointment";
-        }
-
-        @GetMapping("/providerChat")
-        public String showProviderChat() {
-
-            return "providerChat";
-        }
-
-        @GetMapping("/providerPatientChat")
-        public String showProviderPatientChat() {
-
-            return "providerPatientChat";
-        }
-
-         @GetMapping("/providerAdminChat")
-        public String showProviderAdminChat() {
-
-            return "providerAdminChat";
-        }
-
-        @GetMapping("/providerPrescriptionRefill")
-        public String showProviderPrescriptionRefill() {
-
-            return "providerPrescriptionRefill";
-        }
-
-        @GetMapping("/providerResults")
-        public String showProviderResults() {
-
-            return "providerResults";
-        }
-
-
+        return "providerLoginPage";
     }
 
+    @GetMapping("/providerLoginPageRetry")
+    public String providerLoginRetry(Model model) {
+        return "providerLoginPageRetry";
+    }
 
+    @PostMapping("/providerLoginPage")
+    public String processLoginForm(@ModelAttribute("providerInfo") ProviderInfo providerInfo) {
+        boolean authenticated = providerService.authenticate(providerInfo.getUsername(), providerInfo.getPassword());
+        if (authenticated && providerInfo.getUserType().equals("provider")) {
+            return "redirect:/providerDashboard";
+        } else {
+            return "redirect:/providerLoginPageRetry";
+        }
+    }
+
+    @GetMapping("/providerDashboard")
+    public String showDashboardPage() {
+
+        return "providerDashboard";
+    }
+
+    @GetMapping("/providerAppointment")
+    public String showAppointmentPage(Model model) {
+        model.addAttribute("providerInfo", new ProviderInfo());
+        return "providerAppointment";
+    }
+
+    @GetMapping("/providerChat")
+    public String showProviderChat() {
+
+        return "providerChat";
+    }
+
+    @GetMapping("/providerPatientChat")
+    public String showProviderPatientChat() {
+
+        return "providerPatientChat";
+    }
+
+    @GetMapping("/providerAdminChat")
+    public String showProviderAdminChat() {
+
+        return "providerAdminChat";
+    }
+
+    @GetMapping("/providerPrescriptionRefill")
+    public String showProviderPrescriptionRefill() {
+
+        return "providerPrescriptionRefill";
+    }
+
+    @GetMapping("/providerResults")
+    public String showProviderResults(Model model) {
+        model.addAttribute("resultInfo", new ResultInfo());
+        return "providerResults";
+    }
+
+    @PostMapping("/providerResults")
+    public String processProviderResultsForm(@ModelAttribute("resultInfo") ResultInfo resultInfo) {
+        // Extract the data from the ResultInfo object
+        String patientName = resultInfo.getPatientName();
+        Date date = resultInfo.getDate();
+        String description = resultInfo.getDescription();
+
+        // Create a ResultData object with the extracted data
+        ResultData resultData = new ResultData(patientName, date, description);
+
+        // Call the ProviderService to process and store the ResultData object in the database
+        providerService.processResults(resultData);
+
+        // Redirect to a success page or return an appropriate response
+        return "redirect:/successPage";
+    }
+}
---
+++ CareNet/CareNet/src/main/java/com/csc340sp23/CareNet/provider/ResultInfo.java
@@ -0,0 +1,45 @@
+package com.csc340sp23.CareNet.provider;
+
+import java.util.Date;
+
+public class ResultInfo {
+
+    private String patientName;
+    private Date date;
+    private String description;
+
+    // Constructors
+    public ResultInfo() {
+    }
+
+    public ResultInfo(String patientName, Date date, String description) {
+        this.patientName = patientName;
+        this.date = date;
+        this.description = description;
+    }
+
+    // Getters and Setters
+    public String getPatientName() {
+        return patientName;
+    }
+
+    public void setPatientName(String patientName) {
+        this.patientName = patientName;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
